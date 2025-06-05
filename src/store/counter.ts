c6e9a0d00b20707732891ef7437791f4bcdac89c
Kirill Kuznetsov <kdevlab@yandex.ru>
2025-06-05 13:49:06 +0000

    Initial commit

---
+++ src/store/counter.ts
@@ -0,0 +1,13 @@
+import { defineStore } from "pinia";
+import { computed, ref } from "vue";
+
+export const useCounterStore = defineStore("counter", () => {
+  const count = ref(0);
+  const double = computed(() => count.value * 2);
+
+  function increment() {
+    count.value++;
+  }
+
+  return { count, double, increment };
+});
---
+++ vite.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+import vue from "@vitejs/plugin-vue";
+import tailwindcss from "@tailwindcss/vite";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [tsconfigPaths({ loose: true }), vue(), tailwindcss()],
+});
