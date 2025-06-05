b71e902f5120862c7049e3ec552b37d185411d49
Kirill Kuznetsov <kdevlab@yandex.ru>
2025-06-05 15:36:10 +0000

    Add mkcert for https support

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,18 @@
+import os from "node:os";
+import path from "node:path";
+
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import vue from "@vitejs/plugin-vue";
 import tailwindcss from "@tailwindcss/vite";
+import mkcert from "vite-plugin-mkcert";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [tsconfigPaths({ loose: true }), vue(), tailwindcss()],
+  plugins: [
+    vue(),
+    tsconfigPaths({ loose: true }),
+    tailwindcss(),
+    mkcert({ mkcertPath: "/usr/bin/mkcert", savePath: path.join(os.homedir(), ".config", "vite-plugin-mkcert") }),
+  ],
 });
