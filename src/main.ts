02189a7d59afa44e80be699a3547a1b3344abefd
Kirill Kuznetsov <kdevlab@yandex.ru>
2025-08-01 23:51:50 +0300

    Replace vite inspect to vue inspect

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,13 @@ import "@/style.css";
 
 import { createApp } from "vue";
 import { createPinia } from "pinia";
+import { PiniaColada } from "@pinia/colada";
 
 import App from "@/App.vue";
 
 const pinia = createPinia();
 const app = createApp(App);
 app.use(pinia);
+app.use(PiniaColada, {});
 
 app.mount("#app");
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import vue from "@vitejs/plugin-vue";
-import inspect from "vite-plugin-inspect";
 import UnoCSS from "unocss/vite";
+import vueDevTools from 'vite-plugin-vue-devtools';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [inspect(), UnoCSS(), vue(), tsconfigPaths({ loose: true })],
+  plugins: [vueDevTools(), UnoCSS(), vue(), tsconfigPaths({ loose: true })],
 });
